ca5e94fcf37fc4fd9b6abbd6a56b6d4729f822f3
petar2405@gmail.com <Natgeo24>
2018-01-28 23:14:45 +0100

    modification

--- BetterSports.java
+++ BetterSports.java
@@ -3,6 +3,7 @@ public enum BetterSports {
 	KOSARKA {public int weight(int s) {return s*5;}},
 	FUDBAL {public int weight(int s) {return 20*s;}},
 	KRIKET {public int weight(int s) {return 3*4;}},
+	SKVOSH {public int weight(int s) {return 34*s;}},
 	VESLANJE {public int weight(int s) {return 32*s;}};
 	
 	public abstract int weight(int s);
